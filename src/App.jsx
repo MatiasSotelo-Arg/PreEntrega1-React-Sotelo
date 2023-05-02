a5476ba31fef2229bea3f0baef1d62d4013cc7a2
Matias Sotelo <matysotelo07@gmail.com>
2023-05-01 23:51:55 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <>
       <div className='App'>
         <NavBar />
-        <ItemListContainer greeting={'Bienvenidos'}/>
+        <ItemListContainer greeting={'Bienvenidos!'}/>
       </div>
     </>
   )
